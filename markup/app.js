6b83a1523090552c50dba72dca449e58cd07ce96
ajhandler <alexjhandler@gmail.com>
2016-06-24 10:42:07 -0400

    add mockups

--- markup/app.js
+++ markup/app.js
@@ -4,3 +4,15 @@ $('.nav__item').on("click", function(e){
   $('.nav__item--active').addClass('nav__item').removeClass('nav__item--active');
   $(this).addClass('nav__item--active');
 });
+
+function scrollToElement(element,target){
+  $(element).on("click",function(e){
+    // alert("it is clicking")
+    $('html,body').animate({
+      scrollTop: $(target).offset().top
+    }, 2000);
+  });
+}
+scrollToElement("#scrollAgenda", "#agenda");
+scrollToElement("#scrollSpeakers", "#speakers");
+scrollToElement("#scrollAbout", "#about");
